39d5aeb8462b165fb25aaa590398196801e9c8d9
telnaggar2 <tarik.elnaggar2@dxc.com>
2024-12-18 03:18:21 +0200

    add product-detail component

---
+++ src/app/components/product-detail/product-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterModule],
+  templateUrl: './product-detail.component.html'
+})
+export class ProductDetailComponent implements OnInit {
+  product?: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+}
